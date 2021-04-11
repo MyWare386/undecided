e354a0a62fd440cde9516a7c91c7434663e8780e
Martin DROULEZ <38752508+MyWare386@users.noreply.github.com>
2021-04-11 11:19:51 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="x-ua-compatible" content="ie=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <title>Undecided ?</title>
+  <link rel="stylesheet" href="css/main.css" />
+  <link rel="icon" href="images/favicon.png" />
+
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Fascinate&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+  <header>
+    <center>
+      <a href="index.php"><img src="images/banniere.png" style="width:20%;" /></a>
+    </center>
+  </header>
+
+  <section id="info">
+
+    <h1></h1>
+
+    <p>
+
+    </p>
+
+  </section>
+
+  <section id="choices">
+
+    <div class="choice">
+
+    </div>
+
+    <div class="choice">
+
+    </div>
+
+  </section>
+
+</body>
+</html>
