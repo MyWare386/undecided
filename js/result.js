61608c23a971c3a403eb96404187c9c57f62ba40
Martin DROULEZ <38752508+MyWare386@users.noreply.github.com>
2021-04-12 19:08:27 +0200

    Add files via upload

---
+++ js/result.js
@@ -0,0 +1,15 @@
+window.onload = function()
+{
+  setTimeout(function()
+  {
+    document.getElementById("result_icon2").style.display = "block";
+  }, 3000);
+  setTimeout(function()
+  {
+    document.getElementById("result").style.display = "block";
+  }, 3000);
+  setTimeout(function()
+  {
+    document.getElementById("result_icon").style.display = "block";
+  }, 3000);
+}
